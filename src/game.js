37df643ea4c12e143f33d717c206fb3ea8047c93
Ziv <ziv.perry@zoominfo.com>
2022-11-20 12:44:47 +0200

    test

--- src/game.js
+++ src/game.js
@@ -1,13 +1,9 @@
 console.log('Running game.js starts');
-// const mains = ['welcome', 'players', 'game', 'scores'];
-// helpers functions (shortcuts)
-const qas = document.querySelectorAll.bind(document);
-const byId = document.getElementById.bind(document);
 
 // initial state
 let state = {
     page: 'welcome',
-    players: [],
+    players: [{}, {}],
     rounds: 3,
     currentPlayer: 0,
     x: {}
@@ -16,32 +12,56 @@ console.log('Initial state', state);
 
 // view functions
 
-const hideMains = () => qas('main').forEach(e => e.style.display = 'none');
-const showCurrentPage = (s) => byId(s.page).style.display = 'block';
+const hideMains = () => document.querySelectorAll('main').forEach(e => e.style.display = 'none');
+const showCurrentPage = (s) => document.getElementById(s.page).style.display = 'block';
 
-const playerForm = (n) => {
-    const sec = document.createElement('section');
+const displayPlayerForm = (n/*Index*/) => {
+    console.log('display player');
+
+    // span with text
     const span = document.createElement('span');
+    span.innerHTML = 'Player number #' + n;
+
+    // input with handler
     const inp = document.createElement('input');
+    // inp.addEventListener('keyup', () => {
+    //     setPlayerName(state, n, inp.value);
+    //     console.log(state.players);
+    // });
+
+    // wrapper
+    const sec = document.createElement('section');
     sec.append(span);
     sec.append(inp);
     return sec;
 };
 
-const playersForm = () => {
-
+const displayPlayers = (s) => {
+    console.log('display players');
+    const root = document.querySelector('.players-list');
+    for (let a = 0; a < s.players.length; ++a) {
+        root.appendChild(displayPlayerForm(a));
+    }
 };
 
-function render(s) {
+function render(s /*State*/) {
+    console.log('Render', s);
     hideMains();
     showCurrentPage(s);
 
+    if ('players' === s.page) {
+        displayPlayers(s);
+    }
 }
 
 // actions (data)
 
 function startNewGame(s) {
-    return {...s, page: 'players'};
+    state = {...s, page: 'players'};
+}
+
+function setPlayerName(state, player/*Number*/, name/*String*/) {
+    state.players[player].name = name;
 }
 
 // attach events
@@ -49,7 +69,7 @@ function startNewGame(s) {
 // start button
 const start = document.getElementById('start');
 start.addEventListener('click', () => {
-    state = startNewGame(state);
+    startNewGame(state);
     render(state);
 });
 
